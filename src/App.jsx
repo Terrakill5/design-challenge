073c1be59919f9c035a651f198e246b9bbae2628
JBriceno <josealbertino4@gmail.com>
2023-05-07 07:44:01 -0500

    Agregado mas del footer

--- src/App.jsx
+++ src/App.jsx
@@ -108,19 +108,66 @@ function App() {
       <footer className="footer">
         <div className="formas-pago">
           <div className="formas-pago-ordenado">
-            <img src='src/resources/visa-and-mastercard-logo-26.png' alt='visa' width='150' height='40' />
-            <img src='src/resources/American-Express-Logo-PNG-File.png' alt='american express' width='75' height='40' />
-            <img src='src/resources/Skrill_logo.png' alt='skrill' width='75' height='40' />
-            <img className="monedas-virtuales" src='src/resources/monedas.PNG' alt='bitcoin' width='115' height='35' />
+            <img
+              src="src/resources/visa-and-mastercard-logo-26.png"
+              alt="visa"
+              width="150"
+              height="40"
+            />
+            <img
+              src="src/resources/American-Express-Logo-PNG-File.png"
+              alt="american express"
+              width="75"
+              height="40"
+            />
+            <img
+              src="src/resources/Skrill_logo.png"
+              alt="skrill"
+              width="75"
+              height="40"
+            />
+            <img
+              className="monedas-virtuales"
+              src="src/resources/monedas.PNG"
+              alt="bitcoin"
+              width="115"
+              height="35"
+            />
             <span className="monedas-virtuales-span">and 50+ more</span>
           </div>
         </div>
         <div className="bottom">
           <div className="redes-sociales">
-            <img src='src/resources/redes.PNG' alt='redes sociales' width='230' height='60' />
+            <img
+              src="src/resources/redes.PNG"
+              alt="redes sociales"
+              width="230"
+              height="60"
+            />
           </div>
           <div className="informacion">
-
+            <div className="informacion-imagen informacion-grid">
+              <span className="subtitulos-footer">support@chicksgold.com</span>
+            </div>
+            <div className="informacion-chicks">
+              <span className="titulo-footer">Chicks Gold</span>
+              <p className="subtitulos-footer">Games</p>
+              <p className="subtitulos-footer">About Us</p>
+              <p className="subtitulos-footer">Blog</p>
+              <p className="subtitulos-footer">Sitemap</p>
+            </div>
+            <div className="informacion-support">
+              <span className="titulo-footer">Support</span>
+              <p className="subtitulos-footer">Contact Us</p>
+              <p className="subtitulos-footer">FAQ</p>
+            </div>
+            <div className="informacion-legal">
+              <span className="titulo-footer">Legal</span>
+              <p className="subtitulos-footer">Privacy Policy</p>
+              <p className="subtitulos-footer">Terms Of Service</p>
+              <p className="subtitulos-footer">Copywrite Policy</p>
+            </div>
+            <div className="informacion-trustpilot subtitulos-footer">Trustpilot Reviews</div>
           </div>
         </div>
       </footer>
