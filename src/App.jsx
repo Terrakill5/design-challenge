deb1a124123cc8e8859b0849de48bc8f9707ced3
JBriceno <josealbertino4@gmail.com>
2023-05-08 08:30:37 -0500

    Agregado los containers de main y casi terminado el header y footer

--- src/App.jsx
+++ src/App.jsx
@@ -52,55 +52,142 @@ function App() {
         </div>
       </header>
       <body className="body">
-        <div className="custom-card">
-          <div className="custom-card-header">
-            <div className="custom-card-header-stock">
-              <span className="on-sale">
-                <span className="dot"></span>ON SALE
-              </span>
-              <span className="in-stock">In stock</span>
-            </div>
-            <div className="custom-card-header-input">
-              <input
-                className="custom-card-header-input-input"
-                value="1"
-                type="number"
-                size="5"
-              />
-            </div>
+        <div className="body-container">
+          <div className="body-title-container">
+            <span className="body-title">Condimentum consectetur</span>
           </div>
-          <div className="custom-card-body">
-            <img
-              src="src/resources/bleach1.png"
-              alt="primer objeto"
-              width="80"
-              height="80"
-            />
-            <div className="custom-card-body-title">
-              <span className="titulo">Blue Partyhat</span>
-              <img
-                src="src/resources/BleachIcon.PNG"
-                alt="bleach icon"
-                width="25"
-                height="25"
-              />
+          <nav className="body-nav">
+            <div className="select-game">
+              <div className="select-game-text">
+                <span className="sword"></span> <span>Select a game</span>
+              </div>
+              <div className="select-game-icon">
+                <span className="caret-down"></span>
+              </div>
             </div>
-            <div className="precio">
-              <span className="precio-actual">$450.00</span>
-              <span className="precio-tachado">$522.50</span>
+            <div className="search-parameters">
+              <div className="search">
+                <span className="magnifying-glass"></span>
+                <span className="search-text">Search</span>
+              </div>
+              <div className="search-price">
+                <div className="search-price-title">
+                  <div>
+                    <span className="bag"></span>
+                  </div>
+                  <div className="search-price-subtitle">
+                    <span className="search-price-price">Price</span>
+                    <span className="search-price-all">All</span>
+                  </div>
+                </div>
+                <div className="select-game-icon">
+                  <span className="caret-down"></span>
+                </div>
+              </div>
+              <div className="search-item">
+                <div className="search-price-title">
+                  <div>
+                    <span className="feather"></span>
+                  </div>
+                  <div className="search-price-subtitle">
+                    <span className="search-price-price">Item Type</span>
+                    <span className="search-price-all">All</span>
+                  </div>
+                </div>
+                <div className="select-game-icon">
+                  <span className="caret-down"></span>
+                </div>
+              </div>
             </div>
-            <div className="body-text">
-              <span className="texto-body">
-                Lorem ipsum dolor sit amet consetetur sadipscing elitr
-              </span>
+          </nav>
+          <div className="card-container">
+            <div className="card-container-header">
+              <div>
+                <span>Showing 20 - from 125</span>
+              </div>
+              <div className="search-item">
+                <div className="search-price-title">
+                  <div>
+                    <span className="feather"></span>
+                  </div>
+                  <div className="search-price-subtitle">
+                    <span className="search-price-price">Sort By</span>
+                    <span className="search-price-all">featured</span>
+                  </div>
+                </div>
+                <div className="select-game-icon">
+                  <span className="caret-down"></span>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="custom-card-footer">
-            <button className="buttons detail-button">Details</button>
-            <button className="buttons add-button">
-              <span className="add-button-span">Add</span>
-              <span className="icon shopping-card"></span>
-            </button>
+            <div className="container">
+              <div className="row">
+                <div className="col-12 col-xs-6 col-md-4 col-lg-3 col-xl-2">
+                  <div className="custom-card">
+                    <div className="custom-card-header">
+                      <div className="custom-card-header-stock">
+                        <span className="on-sale">
+                          <span className="dot"></span>ON SALE
+                        </span>
+                        <span className="in-stock">In stock</span>
+                      </div>
+                      <div className="custom-card-header-input">
+                        <input
+                          className="custom-card-header-input-input"
+                          value="1"
+                          type="number"
+                          size="5"
+                        />
+                      </div>
+                    </div>
+                    <div className="custom-card-body">
+                      <img
+                        src="src/resources/bleach1.png"
+                        alt="primer objeto"
+                        width="80"
+                        height="80"
+                      />
+                      <div className="custom-card-body-title">
+                        <span className="titulo">Blue Partyhat</span>
+                        <img
+                          src="src/resources/BleachIcon.PNG"
+                          alt="bleach icon"
+                          width="25"
+                          height="25"
+                        />
+                      </div>
+                      <div className="precio">
+                        <span className="precio-actual">$450.00</span>
+                        <span className="precio-tachado">$522.50</span>
+                      </div>
+                      <div className="body-text">
+                        <span className="texto-body">
+                          Lorem ipsum dolor sit amet consetetur sadipscing elitr
+                        </span>
+                      </div>
+                    </div>
+                    <div className="custom-card-footer">
+                      <button className="buttons detail-button">Details</button>
+                      <button className="buttons add-button">
+                        <span className="add-button-span">Add</span>
+                        <span className="icon shopping-card"></span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="card-container-footer">
+              <button className="button-move button-transparent"><span className="caret-left"></span></button>
+              <button className="button-active">1</button>
+              <button className="button-transparent">2</button>
+              <button className="button-transparent">3</button>
+              <button className="button-transparent">4</button>
+              <button className="button-transparent">...</button>
+              <button className="button-transparent">11</button>
+              <button className="button-move button-transparent"><span className="caret-right"></span></button>
+            </div>
           </div>
         </div>
       </body>
@@ -147,7 +234,14 @@ function App() {
           </div>
           <div className="informacion">
             <div className="informacion-imagen informacion-grid">
-              <span className="subtitulos-footer">support@chicksgold.com</span>
+              <img
+                className="informacion-imagen-img"
+                src="src/resources/footer1.PNG"
+                alt="chicks gold footer"
+                width="200"
+                height="50"
+              />
+              <p className="subtitulos-footer">support@chicksgold.com</p>
             </div>
             <div className="informacion-chicks">
               <span className="titulo-footer">Chicks Gold</span>
@@ -167,7 +261,47 @@ function App() {
               <p className="subtitulos-footer">Terms Of Service</p>
               <p className="subtitulos-footer">Copywrite Policy</p>
             </div>
-            <div className="informacion-trustpilot subtitulos-footer">Trustpilot Reviews</div>
+            <div className="informacion-trustpilot subtitulos-footer">
+              <img
+                className="trustpilot-img"
+                src="src/resources/star.PNG"
+                alt="trustpilot"
+                width="20"
+                height="20"
+              />
+              <img
+                className="trustpilot-img"
+                src="src/resources/star.PNG"
+                alt="trustpilot"
+                width="20"
+                height="20"
+              />
+              <img
+                className="trustpilot-img"
+                src="src/resources/star.PNG"
+                alt="trustpilot"
+                width="20"
+                height="20"
+              />
+              <img
+                className="trustpilot-img"
+                src="src/resources/star.PNG"
+                alt="trustpilot"
+                width="20"
+                height="20"
+              />
+              <img
+                className="trustpilot-img"
+                src="src/resources/star.PNG"
+                alt="trustpilot"
+                width="20"
+                height="20"
+              />
+              Trustpilot Reviews
+            </div>
+          </div>
+          <div className="rights-footer">
+            <span>© 2017 - 2020 Chicksgold.com- All Rights Reserved</span>
           </div>
         </div>
       </footer>
