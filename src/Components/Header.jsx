6fe565621f1a743bfda5dd6911df6c7309fc7a52
JBriceno <josealbertino4@gmail.com>
2023-05-15 06:21:10 -0500

    Mejora de responsive en el header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,26 +12,31 @@ const Header = () => <header className="header">
     />
   </div>
 
-  <div className="mobile-non">
-    <span className="dropdown">
+  <div className="mobile-non ">
+    <span className="dropdown tablet-non">
       CURRENCY <span className="icon chevron-down"></span>
     </span>
-    <span className="dropdown">
+    <span className="dropdown tablet-non">
       ITEMS <span className="icon chevron-down"></span>
     </span>
-    <span className="dropdown">
+    <span className="dropdown tablet-non">
       ACCOUNTS <span className="icon chevron-down"></span>
     </span>
-    <span className="dropdown">
+    <span className="dropdown tablet-non">
       SERVICES <span className="icon chevron-down"></span>
     </span>
-    <span className="dropdown">
+    <span className="dropdown tablet-non">
       SWAP <span className="icon chevron-down"></span>
     </span>
-    <span className="dropdown">
+    <span className="dropdown tablet-non">
       SELL <span className="icon chevron-down"></span>
     </span>
+    <div className="tablet menu-header">
+    <span className="menu"></span>
+
   </div>
+  </div>
+ 
 </div>
 <div className="sign-in-header mobile-non">
   <div>
